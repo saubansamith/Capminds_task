ae6d2119ee015d6d488d65a628f5cb35a5c9d0f1
Mohammed Sauban Samith P <86638217+saubansamith@users.noreply.github.com>
2026-02-17 12:34:02 +0530

    Update index.php

--- AES/public/index.php
+++ AES/public/index.php
@@ -1,5 +1,7 @@
 <?php
 // ---------------- LOAD REQUIRED FILES ----------------
+
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
@@ -7,7 +9,7 @@ $basePath = dirname(__DIR__);
 
 require_once $basePath . '/app/helpers/Response.php';
 require_once $basePath . '/app/helpers/JWT.php';
-require_once $basePath . '/app/helpers/Csrf.php';
+require_once $basePath . '/app/helpers/csrf.php';
 require_once $basePath . '/app/middleware/JsonMiddleware.php';
 require_once $basePath . '/app/middleware/AuthMiddleware.php';
 require_once $basePath . '/app/middleware/CsrfMiddleware.php';
@@ -20,12 +22,16 @@ require_once $basePath . '/app/controllers/PatientController.php';
 
 // ---------------- LOAD .env VARIABLES ----------------
 $envPath = __DIR__ . '/../.env';
-$lines = file($envPath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-foreach ($lines as $line) {
-    if (strpos(trim($line), '#') === 0) continue;
-    list($key, $value) = explode('=', $line, 2);
-    $_ENV[trim($key)] = trim($value);
+if (file_exists($envPath)) {
+    $lines = file($envPath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) continue;
+
+        list($name, $value) = explode('=', $line, 2);
+        $_ENV[trim($name)] = trim($value);
+    }
 }
 
 // ---------------- RUN JSON MIDDLEWARE ----------------
