2462104952cfb8eee231089416f0a664f04413c3
Mohammed Sauban Samith P <86638217+saubansamith@users.noreply.github.com>
2026-02-17 11:52:36 +0530

    Add AES encryption and decryption methods

--- AES/app/helpers/BlindIndex.php
+++ AES/app/helpers/BlindIndex.php
@@ -1,27 +1,55 @@
 <?php
 
-class BlindIndex {
+class AES {
 
-    public static function emailHash($email) {
+    private static function key() {
 
-        $normalized = strtolower(trim($email));
+        if (empty($_ENV['AES_SECRET'])) {
+            throw new Exception("AES_SECRET not defined in .env");
+        }
 
-        return hash_hmac(
-            'sha256',
-            $normalized,
-            $_ENV['HASH_SECRET']
-        );
+        return hash('sha256', $_ENV['AES_SECRET'], true);
     }
 
-    public static function phoneHash($phone)
-{
-    $normalized = preg_replace('/[^0-9]/', '', $phone);
+    public static function encrypt($plaintext) {
 
-    return hash_hmac(
-        'sha256',
-        $normalized,
-        $_ENV['HASH_SECRET']
-    );
-}
+        $cipher = "AES-256-CBC";
 
+        $ivLength = openssl_cipher_iv_length($cipher);
+        $iv = random_bytes($ivLength);
+
+        $encrypted = openssl_encrypt(
+            $plaintext,
+            $cipher,
+            self::key(),
+            OPENSSL_RAW_DATA,
+            $iv
+        );
+
+        return base64_encode($iv . $encrypted);
+    }
+
+    public static function decrypt($encryptedData) {
+
+        $cipher = "AES-256-CBC";
+
+        $data = base64_decode($encryptedData);
+
+        $ivLength = openssl_cipher_iv_length($cipher);
+
+        if (strlen($data) < $ivLength) {
+            return false;
+        }
+
+        $iv = substr($data, 0, $ivLength);
+        $encrypted = substr($data, $ivLength);
+
+        return openssl_decrypt(
+            $encrypted,
+            $cipher,
+            self::key(),
+            OPENSSL_RAW_DATA,
+            $iv
+        );
+    }
 }
