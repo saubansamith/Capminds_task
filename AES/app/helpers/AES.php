d025d16228166303effdb9449894d00725546a1b
Mohammed Sauban Samith P <86638217+saubansamith@users.noreply.github.com>
2026-02-17 11:52:12 +0530

    Refactor AES class for key handling and encryption

--- AES/app/helpers/AES.php
+++ AES/app/helpers/AES.php
@@ -4,21 +4,14 @@ class AES {
 
     private static function key() {
 
-        if (empty($_ENV['AES_SECRET_KEY'])) {
-            throw new Exception("AES_SECRET_KEY not configured");
+        if (empty($_ENV['AES_SECRET'])) {
+            throw new Exception("AES_SECRET not defined in .env");
         }
 
-        // Ensure 32 bytes key for AES-256
-        return hash('sha256', $_ENV['AES_SECRET_KEY'], true);
+        return hash('sha256', $_ENV['AES_SECRET'], true);
     }
 
-    /* ================= ENCRYPT ================= */
-
-    public static function encrypt($plainText) {
-
-        if ($plainText === null || $plainText === '') {
-            return null;
-        }
+    public static function encrypt($plaintext) {
 
         $cipher = "AES-256-CBC";
 
@@ -26,54 +19,37 @@ class AES {
         $iv = random_bytes($ivLength);
 
         $encrypted = openssl_encrypt(
-            $plainText,
+            $plaintext,
             $cipher,
             self::key(),
             OPENSSL_RAW_DATA,
             $iv
         );
 
-        if ($encrypted === false) {
-            throw new Exception("Encryption failed");
-        }
-
         return base64_encode($iv . $encrypted);
     }
 
-    /* ================= DECRYPT ================= */
-
     public static function decrypt($encryptedData) {
 
-        if (empty($encryptedData)) {
-            return null;
-        }
-
         $cipher = "AES-256-CBC";
-        $key = self::key();
 
-        $data = base64_decode($encryptedData, true);
-
-        if ($data === false) {
-            return null;
-        }
+        $data = base64_decode($encryptedData);
 
         $ivLength = openssl_cipher_iv_length($cipher);
 
         if (strlen($data) < $ivLength) {
-            return null;
+            return false;
         }
 
         $iv = substr($data, 0, $ivLength);
-        $cipherText = substr($data, $ivLength);
+        $encrypted = substr($data, $ivLength);
 
-        $decrypted = openssl_decrypt(
-            $cipherText,
+        return openssl_decrypt(
+            $encrypted,
             $cipher,
-            $key,
+            self::key(),
             OPENSSL_RAW_DATA,
             $iv
         );
-
-        return $decrypted === false ? null : $decrypted;
     }
 }
